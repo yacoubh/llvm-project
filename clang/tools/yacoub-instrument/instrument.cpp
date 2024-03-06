ab2ad31767a2ffc8a8e3fa89f258324dc39e0eac
hossaiy <hossaiy@hossaiys-Mini.cust.communityfibre.co.uk>
2024-03-06 10:56:18 -0800

    Add recursive AST visitor

--- clang/tools/yacoub-instrument/instrument.cpp
+++ clang/tools/yacoub-instrument/instrument.cpp
@@ -1,32 +1,39 @@
+#include <clang/Frontend/CompilerInstance.h>
 #include <clang/Tooling/CommonOptionsParser.h>
 #include <clang/Tooling/Tooling.h>
 #include <clang/ASTMatchers/ASTMatchFinder.h>
 #include <clang/Rewrite/Core/Rewriter.h>
+#include <clang/AST/Mangle.h>
+#include <clang/AST/AST.h>
+#include <clang/AST/RecursiveASTVisitor.h>
 
 using namespace clang;
 using namespace clang::ast_matchers;
 using namespace clang::tooling;
 
+class YVisitor : public RecursiveASTVisitor<YVisitor> {
+public:
+    YVisitor(std::string FName, Rewriter &Rewrite) : FunctionName(FName), Rewrite(Rewrite) {}
+    bool VisitCompoundStmt(CompoundStmt *S){
+        SourceLocation start = S->getBeginLoc();
+        Rewrite.InsertTextAfterToken(start, "//func(\"" + FunctionName + "\", ID);");
+        return true;
+    }
+private:
+
+    std::string FunctionName;
+    Rewriter &Rewrite;
+};
+
 class BlockHandler : public MatchFinder::MatchCallback {
 public:
   BlockHandler(Rewriter &Rewrite) : Rewrite(Rewrite) {}
-    virtual void run(const MatchFinder::MatchResult &Result) {
+    
+    virtual void run(const MatchFinder::MatchResult &Result) override {
       const FunctionDecl *FD = Result.Nodes.getNodeAs<FunctionDecl>("func");
       if (FD && FD->hasBody()) {
-        std::string MangledName;
-        llvm::raw_string_ostream MangledNameStream(MangledName);
-        MangleContext *MC = FD->getASTContext().createMangleContext();
-        if (MC->shouldMangleDeclName(FD)) {
-          MC->mangleName(FD, MangledNameStream);
-          MangledNameStream.flush();
-        }
-        delete MC;
-        for (const Stmt *S : FD->getBody()->children()) {
-          if (const CompoundStmt *CS = dyn_cast<CompoundStmt>(S)) {
-            SourceLocation Loc = CS->getLBracLoc();
-            Rewrite.InsertText(Loc, " func1(\"" + MangledName + "\");", true, true);
-          }
-        }
+          YVisitor visitor(FD->getQualifiedNameAsString(), Rewrite);
+          visitor.TraverseStmt(FD->getBody());
       }
     }
 private:
@@ -36,8 +43,11 @@ private:
 class YASTConsumer : public ASTConsumer {
 public:
   YASTConsumer(Rewriter &R) : handleBlock(R) {
-    this->Matcher.addMatcher(functionDecl(isExpansionInMainFile()).bind("func"), &Handler);
+    this->Matcher.addMatcher(functionDecl(isExpansionInMainFile()).bind("func"), &handleBlock);
   }
+    void HandleTranslationUnit(ASTContext &Context) override {
+        Matcher.matchAST(Context);
+    }
 private:
   BlockHandler handleBlock;
   MatchFinder Matcher;
@@ -45,14 +55,14 @@ private:
 
 class YFrontEndAction : public ASTFrontendAction {
 public:
-  YFrontEndAction() {}
   void EndSourceFileAction() override {
     TheRewriter.overwriteChangedFiles();
   }
+
   std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &CI,
                                                  StringRef file) override {
     TheRewriter.setSourceMgr(CI.getSourceManager(), CI.getLangOpts());
-    return llvm::make_unique<YASTConsumer>(TheRewriter);
+    return std::make_unique<YASTConsumer>(TheRewriter);
   }
 private:
   Rewriter TheRewriter;
